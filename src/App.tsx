57d5a93698066802c527d1938a562d22e3db004f
[dyad] <clahshop33@gmail.com>
2025-10-25 16:00:06 +0200

    [dyad] Réactivation progressive des fournisseurs de contexte - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
+import { CustomToaster } from "@/components/CustomToaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Accueil from "./pages/Accueil";
+import LeClub from "./pages/LeClub";
+import CompetitionsEquipes from "./pages/CompetitionsEquipes";
+import Adhesions from "./pages/Adhesions";
+import Boutique from "./pages/Boutique";
+import Partenaires from "./pages/Partenaires";
+import Contact from "./pages/Contact";
+import TournamentRegistration from "./pages/TournamentRegistration";
+import TournamentRegistrationsList from "./pages/TournamentRegistrationsList";
+import ClassementJoueurs from "./pages/ClassementJoueurs";
+import NotFound from "./pages/NotFound";
+import { LightboxProvider } from "./context/LightboxContext";
 import React from "react";
+
+const queryClient = new QueryClient();
 
 const App = () => (
   <div className="app-root">
-    <h1>Application en cours de débogage...</h1>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        {/* Les autres composants seront réintroduits progressivement */}
+        <h1>Application en cours de débogage... (Providers réactivés)</h1>
+      </TooltipProvider>
+    </QueryClientProvider>
+    {/* CustomToaster et Sonner restent désactivés pour le moment */}
+    {/* <CustomToaster /> */}
+    {/* <Sonner /> */}
   </div>
 );
 
