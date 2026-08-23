e3bc170dd08fa0963297d17215ff9ddf806cb2f2
tonioping <clahshop33@gmail.com>
2026-08-23 10:56:16 +0200

    Ajoute la gestion des groupes d entrainement

--- src/App.tsx
+++ src/App.tsx
@@ -45,6 +45,7 @@ const DemandeDonnees = React.lazy(() => import("./pages/DemandeDonnees"));
 const Administration = React.lazy(() => import("./pages/Administration"));
 const TeamOne = React.lazy(() => import("./pages/TeamOne"));
 const StageAout = React.lazy(() => import("./pages/StageAout"));
+const TrainingGroups = React.lazy(() => import("./pages/TrainingGroups"));
 
 const StatistiquesJoueurs = React.lazy(
   () => import("./pages/StatistiquesJoueurs"),
@@ -69,6 +70,7 @@ const App = () => (
                 <Route path="/competitions-equipes/criterium-gironde" element={<CriteriumGironde />} />
                 <Route path="/classement-joueurs" element={<ClassementJoueurs />} />
                 <Route path="/stage-aout" element={<StageAout />} />
+                <Route path="/groupes-entrainement" element={<TrainingGroups />} />
                 <Route path="/classement-joueurs/:licence" element={<FicheJoueur />} />
                 <Route path="/classement-joueurs/progression-mensuelle" element={<ProgressionMensuelle />} />
                 <Route path="/classement-joueurs/progression-annuelle" element={<ProgressionAnnuelle />} />
